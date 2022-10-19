99523f0cd332af1bad03846b265f046bd343f3dc
lihao25 <15937811122@163.com>
2022-10-20 01:50:01 +0800

    iterator mode

---
+++ src/iterator/MyAggregate.java
@@ -0,0 +1,17 @@
+package src.iterator;
+
+
+public class MyAggregate implements Aggregate{
+
+    Object[] objects;
+
+    MyAggregate(Object[] objects){
+        this.objects = objects;
+    }
+
+
+    @Override
+    public Iterator createAggregate() {
+        return new MyIterator(objects);
+    }
+}
---
+++ src/iterator/MyIterator.java
@@ -0,0 +1,25 @@
+package src.iterator;
+
+public class MyIterator implements Iterator{
+
+    private Object[] objects;
+
+    private int position;
+
+    MyIterator(Object[] objects){
+        this.objects = objects;
+    }
+
+    @Override
+    public Object next() {
+        return objects[position++];
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(position >= objects.length){
+            return false;
+        }
+        return true;
+    }
+}
